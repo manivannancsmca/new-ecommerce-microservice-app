0339c8778c38e081c0e691d6fce0d8920df08484
manivannancsmca <manivannancsmca@gmail.com>
2026-07-28 16:12:18 +0530

    agentic-ai-service application developed

---
+++ agentic-ai-service/src/main/java/com/agentic_ai_service/AgenticAiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agentic_ai_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenticAiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenticAiServiceApplication.class, args);
+	}
+
+}
---
+++ agentic-ai-service/src/test/java/com/agentic_ai_service/AgenticAiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agentic_ai_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenticAiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
